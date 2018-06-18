087695b46df0edfa33d90a43e03d63d6cbfb2200
omar_clueapps14 <omar.ios@clueapps.net>
2018-06-18 11:09:36 +0200

    build basics in App

---
+++ SunAndSet/Helper/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  SunAndSet
+//
+//  Created by User on 6/14/18.
+//  Copyright © 2018 clueapps. All rights reserved.
+//
+
+import UIKit
+
+class Constants: NSObject {
+
+}
---
+++ SunAndSet/Helper/Extensions.swift
@@ -0,0 +1,13 @@
+//
+//  Extensions.swift
+//  SunAndSet
+//
+//  Created by User on 6/14/18.
+//  Copyright © 2018 clueapps. All rights reserved.
+//
+
+import UIKit
+
+class Extensions: NSObject {
+
+}
